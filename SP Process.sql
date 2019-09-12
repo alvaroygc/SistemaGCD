d2e27b55d1df117a4bc3207eaea2b98965dd5f97
Alvaroygc <alvarogudiel@gmail.com>
2019-09-12 13:18:31 -0600

    Back End Case

---
+++ SP Process.sql
@@ -0,0 +1,73 @@
+Select  * From Process
+
+Create Procedure SP_Insert_Status
+
+@Name Varchar(60),
+@Description Varchar (50)
+
+ As
+
+	Begin Transaction
+			Begin Try
+	    	Insert Into Status (Name,Description)
+			Values (@Name, @Description)
+		Commit
+		End Try
+		Begin Catch
+		Rollback
+		Insert Into Logs (Name_Procedure, Message) Values (object_name(@@PROCID),ERROR_MESSAGE())
+		Print 'Error en la Transaccion'
+		End Catch
+
+
+Create Procedure SP_Delete_Status
+
+@Id int
+
+ As
+
+	Begin Transaction
+			Begin Try
+	    	Delete From Status where Id=@Id
+		Commit
+		End Try
+		Begin Catch
+		Rollback
+		Insert Into Logs (Name_Procedure, Message) Values (object_name(@@PROCID),ERROR_MESSAGE())
+		Print 'Error en la Transaccion'
+		End Catch
+
+Create Procedure SP_Select_Status
+
+ As
+
+	Begin Transaction
+			Begin Try
+	    	Select Id, Name, Description, Created_On From Status
+		Commit
+		End Try
+		Begin Catch
+		Rollback
+		Insert Into Logs (Name_Procedure, Message) Values (object_name(@@PROCID),ERROR_MESSAGE())
+		Print 'Error en la Transaccion'
+		End Catch
+
+
+Create Procedure SP_Update_Status
+
+@Id Int,
+@Name Varchar (60),
+@Description Varchar (60)
+
+ As
+
+	Begin Transaction
+			Begin Try
+	    	Update Status Set Name=@Name, Description=@Description Where Id=@Id
+		Commit
+		End Try
+		Begin Catch
+		Rollback
+		Insert Into Logs (Name_Procedure, Message) Values (object_name(@@PROCID),ERROR_MESSAGE())
+		Print 'Error en la Transaccion'
+		End Catch
---
+++ SistemaGCD/wwwroot/js/Cases.js
@@ -0,0 +1,145 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        modalVisibility: 'none',
+        disabledButton: false,
+        cases: [],
+        status: [],
+        selectedCases: {},
+        editModCases: '',
+        gridCases: '',
+        inputErrors: []
+    },
+
+    mounted: function () {
+
+    },
+
+    created: function () {
+        var self = this;
+        self.getCases();
+        self.getStatus();
+    },
+
+    methods: {
+        getCases: function () {
+            fetch('./api/Cases/getall')
+                .then(function (response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' + response.status);
+                        return;
+                    }
+                    response.json().then(function (data) {
+                        app.cases = data;
+                    });
+                }
+                )
+                .catch(function (err) {
+                    console.log('Fetch Error :-S', err);
+                });
+        },
+
+        getStatus: function () {
+            fetch('./api/Status/getall')
+                .then(function (response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' + response.status);
+                        return;
+                    }
+                    response.json().then(function (data) {
+                        app.status = data;
+                    });
+                }
+                )
+                .catch(function (err) {
+                    console.log('Fetch Error :-S', err);
+                });
+        },
+
+        openEditModal: function (action, mode) {
+            app.modalVisibility = "block"
+            app.editModCases = mode
+            if (mode == "EDIT") {
+                app.selectedCases = Object.assign({}, action)
+            }
+            if (mode == "NEW") {
+                app.selectedCases = { name: '', description: '' }
+            }
+        },
+        closeModal: function () {
+            app.modalVisibility = "none"
+        },
+
+        validateActionInput: function () {
+            app.inputErrors = []
+            if (app.selectedCases.name == '') {
+                app.inputErrors.push('El nombre no debe ser vacío!')
+            }
+            if (app.selectedCases.description == '') {
+                app.inputErrors.push('Ingrese una descripcion!');
+            }
+        },
+
+        saveEdit: function () {
+            var res = ''
+            //validar
+            app.validateActionInput();
+            if (app.inputErrors.length > 0) {
+                return;
+            }
+            if (app.editModCases == "EDIT") {
+                res = '/api/Cases/update'
+            }
+            if (app.editModCases == "NEW") {
+                res = '/api/Cases/create'
+            }
+            fetch(res, {
+                method: 'post',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(app.selectedCases)
+            })
+                .then(function (r) {
+                    return r.json()
+                })
+                .then(function (data) {
+                    app.closeModal()
+                    app.getCases()
+                })
+                .catch(function (error) {
+                    console.log('Request failed', error);
+                });
+        },
+
+        DeleteCases: function (action) {
+            app.modalVisibility = "block"
+            app.selectedCases = Object.assign({}, action)
+            Vue.nextTick(function () {
+                //Esto se ejecuta en la proxima iteracion de dibujado de elementos en el DOM.
+                if (!confirm("¿Está seguro de eliminar?")) {
+                    app.closeModal()
+                    return;
+                }
+                fetch('/api/Cases/Delete', {
+                    method: 'post',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(app.selectedCases)
+                })
+                    .then(function (r) {
+                        return r.json()
+                    })
+                    .then(function (data) {
+                        //alert(data.result)
+                        app.closeModal()
+                        app.getCases()
+                    })
+                    .catch(function (error) {
+                        console.log('Request failed', error);
+                    });
+            })
+        }
+    }
+})
